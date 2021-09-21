7c7a5c747006fba97891a3782b33fd1e7e85c931
mohammadabd0 <mohammadabdo518@gmail.com>
2021-09-21 18:08:59 +0300

    notfix

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const server = express();
+
+const weatherData = require('./data/weather.json');
+
+const PORT = process.env.PORT;
+server.use(cors());
+
+class Waether {
+    constructor(date,description){
+        this.date = date;
+        this.description =description;
+    }
+
+}
+
+
+// localhost:3002/weather?withercity=
+server.get('/weather', (req, res) =>
+{
+  
+    let withercity = weatherData.find(item =>
+    {if (item.city_name.toLocaleLowerCase()=== req.query.city)
+    {return item;
+    }
+    });
+    let newArray = withercity.data.map(element => {
+      return new Waether(element.valid_date, element.weather.description); });
+    res.status(200).send(newArray);
+
+});
+
+// localhost:3005/ANYTHING
+server.get('*',(req,res)=>{
+    res.status(404).send('route is not found')
+})
+
+server.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`)
+})
+
+
+
+
+
