da5ecbe47106ee379e596558f30324ab6bf09dde
mohammadabd0 <mohammadabdo518@gmail.com>
2021-09-21 19:47:11 +0300

    notfix

--- server.js
+++ server.js
@@ -20,20 +20,39 @@ class Waether {
 
 
 // localhost:3002/weather?withercity=
-server.get('/weather', (req, res) =>
-{
+// server.get('/weather', (req, res) =>
+// {
   
-    let withercity = weatherData.find(item =>
-    {if (item.city_name.toLocaleLowerCase()=== req.query.city)
-    {return item;
-    }
+//     let withercity = weatherData.find(item =>
+//     {if (item.city_name.toLocaleLowerCase()=== req.query.city)
+//     {return item;
+//     }
+//     });
+//     let newArray = withercity.data.map(element => {
+//       return new Waether(element.valid_date, element.weather.description); });
+//     res.status(200).send(newArray);
+
+// });
+
+// localhost:3005/weather?city=
+server.get('/weather',(req,res)=>{
+    // res.send(pokeData);
+
+    let weathercity = req.query.city;
+
+    let Infocity = weatherData.find((item)=>{
+        if(item.city_name.toLocaleLowerCase() === weathercity) {
+            return item
+        }
+        
     });
-    let newArray = withercity.data.map(element => {
-      return new Waether(element.valid_date, element.weather.description); });
-    res.status(200).send(newArray);
+    let newArray = Infocity.data.map(element => {
+              return new Waether(element.valid_date, element.weather.description); });
+            res.status(200).send(newArray);
 
-});
+})
 
+//////
 // localhost:3005/ANYTHING
 server.get('*',(req,res)=>{
     res.status(404).send('route is not found')
