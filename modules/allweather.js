209159ea5ca43aac161aeaa73667842f6ac45d62
mohammadabd0 <mohammadabdo518@gmail.com>
2021-09-25 19:18:33 +0300

    updatelab10

--- modules/allweather.js
+++ modules/allweather.js
@@ -1,33 +1,39 @@
 const axios = require('axios');
-
+let cacheMemory = {};
 
 
 function getweatherHandler(req, res) {
-    
     let weathercity = req.query.city;
     // http://api.weatherbit.io/v2.0/forecast/daily?city=${weathercity}&key=
     //http://localhost:3005/weather?city=Amman
+
     let URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weathercity}&key=${process.env.WEATHER_KEY}`
     console.log(URL);
-    
-    axios.get(URL).then(weatherResults => {
-     let newArray = weatherResults.data.data.map(element => {
+    if (cacheMemory[weathercity] !== undefined) {
+        res.send(cacheMemory[weathercity]);
+    }
+    else {
+        try {
+             axios.get(URL).then(weatherResults => {
+
+             let newArray = weatherResults.data.data.map(element => {
             return new Waether(element)
-         })
+                 });
+         cacheMemory[weathercity] = newArray;
             res.send(newArray)
-        }).catch(error =>{
-            res.send(error)
-        })
 
+        });
+        }
+     catch (error) {
+    res.send(error);
+    }
+    }
 }
-
 
 class Waether {
     constructor(element){
         this.date = element.datetime; 
         this.description =element.weather.description;
     }
-
 }
- 
 module.exports = getweatherHandler;
